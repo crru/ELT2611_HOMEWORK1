ccc1f08028ce48d40177ff19758e1f571687aa43
ELT581568001 <khomkrit.chai@crru.ac.th>
2015-09-23 14:46:14 +0700

    Create ELT581568001.java
    ส่งงานครับๆๆ

---
+++ ELT581568001.java
@@ -0,0 +1,21 @@
+public class Triangle {
+	
+   public static void main(String[] args) {
+	   
+      Scanner scan = new Scanner(System.in); 
+      int height, base;  
+		
+      System.out.print("Please input height : ");
+      height = scan.nextInt();  
+		
+      System.out.print("Please input base : ");
+      base = scan.nextInt();  
+		
+      System.out.println("Calculating Triangle Area");
+      double area = height * base / 2; 
+		
+      System.out.println("Area : " + area);  
+	  
+   }
+   
+}
